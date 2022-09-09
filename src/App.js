309785506812d09dabcf753d5b0f7d3b037c3921
TrungHien <trunghien4100@gmail.com>
2022-09-10 06:55:01 +0700

    new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-container">
+        <button className='btn btn-primary'>Click me!</button>
+    </div>
+  );
+}
+
+export default App;
+
